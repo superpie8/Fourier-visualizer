d20b6c18615fa56eda37d8253b7ffa2a4f455db6
superpie8 <72939944+superpie8@users.noreply.github.com>
2020-11-08 00:45:31 -0800

    initial upload of main two files
    just uploading the main and support files

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  5 11:45:03 2020
+
+@author: super
+
+Solves and plots fourier series and the input function
+"""
+
+import matplotlib
+import modules as m
+
+print(m.base_function(5))
---
+++ modules.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  5 11:44:10 2020
+
+@author: super
+
+modules for the fourier series solver program
+"""
+
+import math
+import numpy as np
+import matplotlib
+
+def base_function(x):
+    return x**3
