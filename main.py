c3015bb31a12c1d7ee8640d13529a26f845cea14
superpie8 <72939944+superpie8@users.noreply.github.com>
2020-11-19 22:16:37 -0800

    Finalizing
    Code works, minor bugfixes to do

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov  5 11:45:03 2020
+Author: Caleb Malaer
+Created: 11/05/2020
+Last Edit: 11/19/2020
 
-@author: super
-
-Solves and plots fourier series and the input function
+Solves for and plots fourier series and the input function
 """
 
 from modules import *
@@ -15,7 +15,7 @@ function = str(input("Input Function: "))
 pfunction = function.replace("^", "**")
 pfunction = sympify(pfunction)
 
-pprint(pfunction)
+"""pprint(pfunction)"""
 
 L = input("L (Interval=[-L, L]): ")
 L = sympify(L)
@@ -23,7 +23,7 @@ L = sympify(L)
 a0 = 1/(2*L) * integrate(pfunction, (x, -L, L))
 an = 1/L * integrate(pfunction * cos(z * x * pi / L), (x, -L, L), conds='none')
 bn = 1/L * integrate(pfunction * sin(z * x * pi / L), (x, -L, L), conds='none')
-print(a0, "|", an, "|", bn)
+"""print(a0, "|", an, "|", bn)"""
 
 sterm = bn * sin(z * x * pi / L)
 cterm = an * cos(z * x * pi / L)
@@ -41,8 +41,13 @@ while n <= N:
     series += " + "
     series += sterm.replace("z", r)
     n += 1
-    print(series)
 
 series = sympify(series)
-print("\n")
+print("\n", "\n", "Series:  ", "\n")
 pprint(series)
+
+F = plot(pfunction, (x,-L, L), show = False, line_color = "black")
+P = plot(series, (x,-L, L), show = False, line_color = "red")
+
+P.append(F[0])
+P.show()
